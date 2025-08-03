86de3d786572268e6e0c748a9d18bfc9aee9d3f6
Mirabbos7 <mirabbosahmedov7777@gmail.com>
2025-08-03 15:24:26 +0500

    Jenkins

---
+++ module-main/src/test/java/com/mjc/school/MainTest.java
@@ -0,0 +1,13 @@
+package com.mjc.school;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MainTest {
+
+    @Test
+    void dummyTest() {
+        assertTrue(true, "This test always passes");
+    }
+}
